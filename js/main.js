29ccfa54814feb0e3185936c5be42925f58cc0ee
Himadri Vyas <himadri.vyas@zucora.com>
2023-02-06 16:47:44 -0500

    adding javascript -1

--- js/main.js
+++ js/main.js
@@ -2,4 +2,19 @@
 'use strict';
     console.log("DOM loaded");
 
+    //change test at interval
+     const wordArray = ['oven','sofa','dish washer']
+
+    //display section
+   function showSection(mainLink, mainSection){
+    document.querySelector(mainLink).addEventListener('click',function(){
+        document.querySelector(mainSection).classList.toggle('d-none');
+        //document.querySelector('.linkCaret').classList.toggle('fa-caret-down');
+        
+    },false);
+   }
+
+   showSection('#showFurnitureSection','#SmartOne-furniture-section');
+   showSection('#showApplianceSection','#SmartOne-appliance-section');
+
 })();
