a460a6e239d1a40ec2333a69cb08aa28a85125a5
Himadri Vyas <himadri.vyas@zucora.com>
2023-02-09 15:29:59 -0500

    review changes in progress

--- js/main.js
+++ js/main.js
@@ -3,18 +3,36 @@
     console.log("DOM loaded");
 
     //change test at interval
-     const wordArray = ['oven','sofa','dish washer']
+     
+
+     //changing text function
+        const target = document.querySelector('#changeWord');
+        const wordArray = ['sofa','fridge','oven'];
+        window.setInterval(function(){
+            const randomNumber = Math.floor(Math.random() * (3 - 0) + 0);
+            target.innerHTML = wordArray[randomNumber] + '?';
+        },1500);
+
+        
 
     //display section
    function showSection(mainLink, mainSection){
     document.querySelector(mainLink).addEventListener('click',function(){
         document.querySelector(mainSection).classList.toggle('d-none');
-        //document.querySelector('.linkCaret').classList.toggle('fa-caret-down');
-        
+        this.children[0].classList.toggle('fa-caret-down');
     },false);
    }
 
    showSection('#showFurnitureSection','#SmartOne-furniture-section');
    showSection('#showApplianceSection','#SmartOne-appliance-section');
+
+   //expand all link clicked
+
+   document.querySelector('#expandAll').addEventListener('click',function(){
+    let AllFAQAccordions = document.querySelectorAll('#FAQAccordion .accordion-collapse');
+    for(const x of AllFAQAccordions){
+        x.classList.add('show');
+    }
+   },false);
 
 })();
